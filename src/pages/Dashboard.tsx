648193c2d05ec5eda816e753dcaa02c46388304e
joschonarth <joschonarth@gmail.com>
2025-04-02 20:20:50 -0300

    feat: implement RefundItem component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 
+import { CATEGORIES } from '@/utils/categories'
 import searchSvg from '@/assets/search.svg'
 
 import { Input } from '@/components/Input'
 import { Button } from '@/components/Button'
+import { RefundItem } from '@/components/RefundItem'
+
+const REFUND_EXAMPLE = {
+  id: '123',
+  name: 'John Doe',
+  category: 'Services',
+  amount: '35,50',
+  categoryImg: CATEGORIES.services.icon,
+}
 
 export function Dashboard() {
   const [name, setName] = useState('')
@@ -31,6 +41,10 @@ export function Dashboard() {
           <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
         </Button>
       </form>
+
+      <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+        <RefundItem data={REFUND_EXAMPLE} />
+      </div>
     </div>
   )
 }
