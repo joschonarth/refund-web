b256c2a6225dead940d64796733dc8b5b5bd62e8
joschonarth <joschonarth@gmail.com>
2025-04-02 17:28:41 -0300

    feat: implement Confirm page

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 import { CATEGORIES, CATEGORIES_KEYS } from '@/utils/categories'
 
@@ -14,8 +15,12 @@ export function Refund() {
   const [isLoading, setIsLoading] = useState(false)
   const [filename, setFilename] = useState<File | null>(null)
 
+  const navigate = useNavigate()
+
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
+
+    navigate('/confirm', { state: { fromSubmit: true } })
   }
 
   return (
