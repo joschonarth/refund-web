ab3b1375cbc2e19cc75610ce7b1be494dfc9b19f
joschonarth <joschonarth@gmail.com>
2025-04-04 15:44:41 -0300

    feat: store user data in context and sync routes with auth state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState } from 'react'
 
 type AuthContext = {
   session: null | UserAPIResponse
+  save: (data: UserAPIResponse) => void
 }
 
 export const AuthContext = createContext({} as AuthContext)
@@ -9,7 +10,13 @@ export const AuthContext = createContext({} as AuthContext)
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null)
 
+  function save(data: UserAPIResponse) {
+    setSession(data)
+  }
+
   return (
-    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, save }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
